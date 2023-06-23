86fec2ef19686ad17eba47152dec10eb7124e5ee
danmatuszak <dmatuszak@ucdavis.edu>
2023-06-22 22:15:26 -0700

    get acc sequence num

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Types, AptosClient } from 'aptos';
 import './App.css';
+
+// Create an AptosClient to interact with devnet.
+const client = new AptosClient('https://fullnode.devnet.aptoslabs.com/v1');
 
 function App() {
   // Retrieve aptos.account on initial render and store it.
-  const [address, setAddress] = React.useState<string | null>(null);
-  
+  const [address, setAddress] = useState<string | null>(null);
+  const [account, setAccount] = useState<Types.AccountData | null>(null);
   /**
    * init function
    */
@@ -13,14 +17,20 @@ function App() {
     const { address, publicKey } = await window.aptos.connect();
     setAddress(address);
   }
-  
-  React.useEffect(() => {
-     init();
+
+  useEffect(() => {
+    init();
   }, []);
+  
+  useEffect(() => {
+    if (!address) return;
+    client.getAccount(address).then(setAccount);
+  }, [address]);
 
   return (
     <div className="App">
       <p>Account Address: <code>{ address }</code></p>
+      <p>Sequence Number: <code>{ account?.sequence_number }</code></p>
     </div>
   );
 }
